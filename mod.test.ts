ced02743b2129e51bc2672c60417de79e4a47777
Nbiba Bedis <bedisnbiba@gmail.com>
2022-12-31 12:06:48 +0100

    add test

---
+++ mod.test.ts
@@ -0,0 +1,38 @@
+import { assertEquals } from "https://deno.land/std@0.170.0/testing/asserts.ts";
+import { path } from "./src/deps.ts";
+import { ChefInternal } from "./src/lib.ts";
+
+class TestChef extends ChefInternal {
+  override Path = path.join(Deno.makeTempDirSync(), "chef");
+}
+
+Deno.test("test chef1", async () => {
+  const dir = Deno.makeTempDirSync();
+  const version = "1.0.0";
+  const exePath = path.join(dir, `exe-${version}`);
+  const versionPath = path.join(dir, "version");
+  Deno.createSync(exePath).close();
+  Deno.writeTextFileSync(versionPath, version);
+
+  const chef = new TestChef();
+
+  chef.addMany([{
+    name: "hello",
+    download: async ({ latestVersion }) => {
+      const exe = `exe-${latestVersion}`;
+      await Deno.copyFile(exePath, exe);
+      await Deno.rename(exe, "exe");
+      return "exe";
+    },
+    version: () => Deno.readTextFile(versionPath),
+  }]);
+
+  await chef.run();
+
+  assertEquals(
+    Deno.readTextFileSync(chef.dbPath),
+    JSON.stringify({ "hello": "1.0.0" }),
+  );
+  // doesn't throw because file exists
+  Deno.readTextFileSync(path.join(chef.BinPath, "hello"));
+});
