955831475da42dd711a7b3dfa10c2f552ff4fbf3
Nbiba Bedis <bedisnbiba@gmail.com>
2022-12-31 12:49:15 +0100

    update tests

--- mod.test.ts
+++ mod.test.ts
@@ -6,38 +6,69 @@ class TestChef extends ChefInternal {
   override Path = path.join(Deno.makeTempDirSync(), "chef");
 }
 
-Deno.test("test chef1", async () => {
+async function withTempDir(f: (dir: string) => Promise<void> | void) {
+  const originalPath = Deno.cwd();
   const dir = Deno.makeTempDirSync();
-  const version = "1.0.0";
-  const exePath = path.join(dir, `exe-${version}`);
-  const versionPath = path.join(dir, "version");
-  Deno.createSync(exePath).close();
-  Deno.writeTextFileSync(versionPath, version);
+  Deno.chdir(dir);
+  try {
+    await f(dir);
+  } finally {
+    Deno.chdir(originalPath);
+  }
+}
+Deno.test("test chef1", async () =>
+  await withTempDir(async (dir: string) => {
+    const version = "1.0.0";
+    const exeCodePath = path.join(dir, `exe-${version}.js`);
+    Deno.writeTextFileSync(
+      exeCodePath,
+      `Deno.writeTextFileSync("./hello", "hello written")`,
+    );
+    await new Deno.Command("deno", {
+      args: ["compile", "--no-check", "--allow-write=.", exeCodePath],
+    }).spawn()
+      .status;
+    const exePath = exeCodePath.replace(".js", "");
 
-  const chef = new TestChef();
+    const versionPath = path.join(dir, "version");
+    Deno.writeTextFileSync(versionPath, version);
 
-  chef.addMany([{
-    name: "hello",
-    download: async ({ latestVersion }) => {
-      const exe = `exe-${latestVersion}`;
-      await Deno.copyFile(exePath, exe);
-      await Deno.rename(exe, "exe");
-      return "exe";
-    },
-    version: () => Deno.readTextFile(versionPath),
-  }]);
+    const chef = new TestChef();
 
-  await chef.run();
+    chef.addMany([{
+      name: "hello",
+      download: async ({ latestVersion }) => {
+        const exe = `exe-${latestVersion}`;
+        await Deno.copyFile(exePath, exe);
+        await Deno.rename(exe, "exe");
+        return "exe";
+      },
+      version: () => Deno.readTextFile(versionPath),
+    }]);
 
-  assertEquals(
-    Deno.readTextFileSync(chef.dbPath),
-    JSON.stringify({ "hello": "1.0.0" }),
-  );
-  // doesn't throw because file exists
-  Deno.readTextFileSync(path.join(chef.BinPath, "hello"));
-});
+    // install hello exe
+    await chef.run([]);
+
+    assertEquals(
+      Deno.readTextFileSync(chef.dbPath),
+      JSON.stringify({ "hello": "1.0.0" }),
+    );
+    // doesn't throw because file exists
+    Deno.readTextFileSync(path.join(chef.BinPath, "hello"));
+
+    // run hello exe
+    await chef.run(["run", "hello"]);
+    // assert it wroks
+    assertEquals(
+      Deno.readTextFileSync(path.join(dir, "hello")),
+      "hello written",
+    );
+  }));
 
 Deno.test("test edit", () => {
   const chef = new TestChef();
-  assertEquals(chef.edit(), `file://${Deno.cwd()}/mod.test.ts`);
+  const expected = Deno.build.os === "windows"
+    ? `file:///${Deno.cwd()}/mod.test.ts`
+    : `file://${Deno.cwd()}/mod.test.ts`;
+  assertEquals(chef.edit(), expected);
 });
