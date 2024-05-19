f7e807fa7551e66a28a92f1020ef79ad4db98937
Nbiba Bedis <bedisnbiba@gmail.com>
2024-05-19 07:14:44 +0100

    document utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,27 @@
+/**
+ * This module provides utility functions to fetch the latest releases from different sources.
+ * @module
+ */
+
+/**
+ * Fetches the latest release tag from a given GitHub repository.
+ *
+ * @param {string} repo - The GitHub repository in the format "owner/repo".
+ * @returns {Promise<string | undefined>} A promise that resolves to the latest release tag, or undefined if the request fails.
+ */
 export async function getLatestGithubRelease(
   repo: string,
 ): Promise<string | undefined> {
-  return await fetch(`https://github.com/${repo}/releases/latest`).then((res) =>
-    res.url.split("/").at(-1)
-  );
+  return await fetch(`https://github.com/${repo}/releases/latest`)
+    .then((res) => res.url.split("/").at(-1));
 }
 
+/**
+ * Fetches the latest version of a given npm package.
+ *
+ * @param {string} packageName - The name of the npm package.
+ * @returns {Promise<string | undefined>} A promise that resolves to the latest version of the package, or undefined if the request fails.
+ */
 export async function getLatestNpmVersion(
   packageName: string,
 ): Promise<string | undefined> {
