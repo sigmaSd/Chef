8c09438ae557ce7e4199ad611cfa0ff0b335f30e
Nbiba Bedis <bedisnbiba@gmail.com>
2024-05-19 07:09:56 +0100

    prepare for jsr

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
-export async function getLatestGithubRelease(repo: string) {
+export async function getLatestGithubRelease(
+  repo: string,
+): Promise<string | undefined> {
   return await fetch(`https://github.com/${repo}/releases/latest`).then((res) =>
     res.url.split("/").at(-1)
   );
@@ -6,12 +8,12 @@ export async function getLatestGithubRelease(repo: string) {
 
 export async function getLatestNpmVersion(
   packageName: string,
-): Promise<string> {
+): Promise<string | undefined> {
   const response = await fetch(
     `https://registry.npmjs.org/${packageName}/latest`,
   );
   if (!response.ok) {
-    throw new Error(`Failed to fetch latest version for ${packageName}`);
+    return;
   }
   const data = await response.json();
   return data.version;
