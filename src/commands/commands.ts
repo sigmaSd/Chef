95aa9f4a669c684a42aa49b805cdc76245ab2468
Nbiba Bedis <bedisnbiba@gmail.com>
2026-02-05 10:53:09 +0100

    update parse lib

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,24 +1,16 @@
-import {
-  Args,
-  argument,
-  cli,
-  command,
-  description,
-  rawRest,
-  required,
-  subCommand,
-  type,
-} from "@sigma/parse";
+import { arg, Args, cli, command, opt, subCommand } from "@sigma/parse";
 
 // Command classes for CLI argument parsing
 @command
 export class RunCommand {
-  @argument({ description: "name of the binary to run" })
-  @type("string")
-  @required()
+  @arg({
+    description: "name of the binary to run",
+    required: true,
+    type: "string",
+  })
   name!: string;
 
-  @rawRest()
+  @arg({ raw: true })
   binArgs: string[] = [];
 }
 
@@ -27,30 +19,34 @@ export class ListCommand {}
 
 @command
 export class UninstallCommand {
-  @argument({ description: "name of the binary to uninstall", rest: true })
-  @type("string[]")
-  @required()
+  @arg({
+    description: "name of the binary to uninstall",
+    rest: true,
+    required: true,
+    type: "string[]",
+  })
   binary: string[] = [];
 }
 
 @command
 export class UpdateCommand {
-  @description("force update a binary")
+  @opt({ description: "force update a binary" })
   force: boolean = false;
 
-  @type("string")
-  @description("skip updating a binary")
+  @opt({ description: "skip updating a binary", type: "string" })
   skip?: string;
 
-  @type("string")
-  @description("only update this binary")
+  @opt({ description: "only update this binary", type: "string" })
   only?: string;
 
-  @description("only look for new versions but don't update")
+  @opt({ description: "only look for new versions but don't update" })
   "dry-run": boolean = false;
 
-  @argument({ description: "name of the binary to update", rest: true })
-  @type("string[]")
+  @arg({
+    description: "name of the binary to update",
+    rest: true,
+    type: "string[]",
+  })
   binary: string[] = [];
 }
 
@@ -62,51 +58,48 @@ export class GuiCommand {}
 
 @command
 export class CreateDesktopCommand {
-  @argument({ description: "name of the binary" })
-  @type("string")
-  @required()
+  @arg({ description: "name of the binary", required: true, type: "string" })
   name!: string;
 
-  @description("set Terminal=true in desktop file")
+  @opt({ description: "set Terminal=true in desktop file" })
   terminal: boolean = false;
 
-  @type("string")
-  @description("set icon path in desktop file")
+  @opt({ description: "set icon path in desktop file", type: "string" })
   icon?: string;
 }
 
 @command
 export class RemoveDesktopCommand {
-  @argument({ description: "name of the binary" })
-  @type("string")
-  @required()
+  @arg({ description: "name of the binary", required: true, type: "string" })
   name!: string;
 }
 
 @command
 export class LinkCommand {
-  @argument({ description: "name of the binary to link" })
-  @type("string")
-  @required()
+  @arg({
+    description: "name of the binary to link",
+    required: true,
+    type: "string",
+  })
   name!: string;
 }
 
 @command
 export class UnlinkCommand {
-  @argument({ description: "name of the binary to unlink" })
-  @type("string")
-  @required()
+  @arg({
+    description: "name of the binary to unlink",
+    required: true,
+    type: "string",
+  })
   name!: string;
 }
 
 @command({ defaultCommand: "help" })
 export class DesktopFileCommand {
-  @subCommand(CreateDesktopCommand)
-  @description("create a desktop file")
+  @subCommand(CreateDesktopCommand, { description: "create a desktop file" })
   create?: CreateDesktopCommand;
 
-  @subCommand(RemoveDesktopCommand)
-  @description("remove a desktop file")
+  @subCommand(RemoveDesktopCommand, { description: "remove a desktop file" })
   remove?: RemoveDesktopCommand;
 }
 
@@ -145,44 +138,38 @@ export interface CommandHandlers {
   exitOnError: false, // Throw errors instead of exiting
 })
 class ChefArgs extends Args {
-  @subCommand(RunCommand)
-  @description("run a binary")
+  @subCommand(RunCommand, { description: "run a binary" })
   run?: RunCommand;
 
-  @subCommand(ListCommand)
-  @description("list installed binaries")
+  @subCommand(ListCommand, { description: "list installed binaries" })
   list?: ListCommand;
 
-  @subCommand(UpdateCommand)
-  @description("install/update binaries")
+  @subCommand(UpdateCommand, { description: "install/update binaries" })
   install?: UpdateCommand;
 
-  @subCommand(UpdateCommand)
-  @description("update installed binaries")
+  @subCommand(UpdateCommand, { description: "update installed binaries" })
   update?: UpdateCommand;
 
-  @subCommand(UninstallCommand)
-  @description("uninstall binaries")
+  @subCommand(UninstallCommand, { description: "uninstall binaries" })
   uninstall?: UninstallCommand;
 
-  @subCommand(EditCommand)
-  @description("output chef entry file")
+  @subCommand(EditCommand, { description: "output chef entry file" })
   edit?: EditCommand;
 
-  @subCommand(GuiCommand)
-  @description("start the gui")
+  @subCommand(GuiCommand, { description: "start the gui" })
   gui?: GuiCommand;
 
-  @subCommand(DesktopFileCommand)
-  @description("manage desktop files")
+  @subCommand(DesktopFileCommand, { description: "manage desktop files" })
   "desktop-file"?: DesktopFileCommand;
 
-  @subCommand(LinkCommand)
-  @description("create symlink to binary in exports directory")
+  @subCommand(LinkCommand, {
+    description: "create symlink to binary in exports directory",
+  })
   link?: LinkCommand;
 
-  @subCommand(UnlinkCommand)
-  @description("remove symlink from exports directory")
+  @subCommand(UnlinkCommand, {
+    description: "remove symlink from exports directory",
+  })
   unlink?: UnlinkCommand;
 }
 
